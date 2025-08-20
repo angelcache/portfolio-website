926bdcdfcc76d37ea3d3e61b972fb1443ff2e68b
Angela <angdkay@gmail.com>
2025-08-20 15:47:16 -0600

    Seperate experience card data from into its own files

---
+++ scripts/data/certificates-data.js
@@ -0,0 +1,26 @@
+export const certificatesCardsInfo = [
+  {
+    title: "comfort quest",
+    img: "./img/experiences/projects1-img.png",
+    info: "Tools Used: Java, Java swing",
+    infoDesc: "A gui mini-game I built while learning Java. You play as a penguin friend helping a burnt-out chicken through fun mini-games: catching bananas, running in a library, and stacking clothes. Each unlocking a happy ending for chicken and a final ending once you finish all three.",
+    github: "./img/icons/github-icon.png",
+    date: "Aug-Dec 2024",
+  },
+  {
+    title: "tweet feed",
+    img: "./img/experiences/projects2-img.png",
+    info: "Tools Used: Java, Java swing",
+    infoDesc: "A Twitter-inspired prototype built with Python, Tkinter, and SQL. Features include posting, replying, retweeting, viewing tweet details, user search, and follower tracking. In a group of 3, I developed the core tweet features and designed the GUI.",
+    github: "./img/icons/github-icon.png",
+    date: "Aug-Dec 2024",
+  },
+  {
+    title: "events lottery app",
+    img: "./img/experiences/projects3-img.png",
+    info: "Tools Used: Java, Android Studio, XML, Firebase",
+    infoDesc: "Android app for managing events through a fair lottery system, built in a group of 6. Entrants can join lotteries, scan QR codes, get notified if selected, confirm attendance, and view event details. Organizers create events, run lotteries, send notifications, generate QR codes, and track participants on a map. I developed all admin features, core entrant functions (nav bar, join/leave, QR codes), and designed + coded the UI.",
+    github: "./img/icons/github-icon.png",
+    date: "Aug-Dec 2024",
+  }
+]
